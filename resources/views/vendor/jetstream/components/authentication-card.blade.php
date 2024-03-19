45d990ea5d2f15aca4a220cae691bb8e4cbcb3d8
Fareed <ucdon1249@gmail.com>
2024-03-19 20:42:16 +0500

    logo update

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,7 +1,7 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
 <div id="logo">
-        <img src="http://humsafar.gedrosia.tech/public/logo.png">
+        <img src="http://zirbar.gedrosia.tech/public/logo.png">
   </div>
     </div>
 
